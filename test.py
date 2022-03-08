a879ec3f0603d871a24fd0a07139a90298ba5eac
sarah-schafer <schafer5@uw.edu>
2022-03-08 00:39:13 -0800

    Corrected filenames

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import pandas as pd
-import question_1
-import question_2
+# import question_1
+# import question_2
 import question_3
-import question_4
+# import question_4
 
 
 
 def main():
-    countries = gpd.read_file('/home/data/countries.geojson')
-    country_wage_data = pd.read_csv('/home/data/gender-wage-gap-oecd.csv')
-    occupations_wage_data = pd.read_csv('/home/data/glassdoor_gender_pay_gap.csv')
+    countries = gpd.read_file('Data/countries.geojson')
+    country_wage_data = pd.read_csv('Data/gender-wage-gap-oecd (1).csv')
+    occupations_wage_data = pd.read_csv('Data/glassdoor_gender_pay_gap.csv')
     question_3.countries_wage_gap_plot(country_wage_data, countries)
-    question_1.occupations_wage_gap_plot(occupations_wage_data)
-    #question_4.time_line_chart_plotly(country_wage_data)
-    #question_2.education_wage_gap_plot(education_wage_data)
+    # question_1.occupations_wage_gap_plot(occupations_wage_data)
+    # question_4.time_line_chart_plotly(country_wage_data)
+    # question_2.education_wage_gap_plot(education_wage_data)
 
 
 if __name__ == '__main__':
