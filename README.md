3540f9c190f958e2103d386ebc86a1bf77622601
sarah-schafer <schafer5@uw.edu>
2022-03-14 10:22:51 -0700

    Add compute summary statistics

--- README.md
+++ README.md
@@ -2,24 +2,24 @@
 
 ## Modules & instructions to run
 
-- `main.py`: run this module to load the three datasets and display the plots 
-in the `analysis.py` module.
+- `main.py`: run this module to load the three datasets and display the plots
+in the `analysis.py` module.  This module also computes and prints basic summary statistics about each dataset that are included in our report.
 
-- `analysis.py`: contains all the functions that generate the plots that answer 
+- `analysis.py`: contains all the functions that generate the plots that answer
 our research questions.
 
-- `test.py`: contains functions that test the data filtering in 
-`analysis.py` by using print statements. All these tests are called in the 
+- `test.py`: contains functions that test the data filtering in
+`analysis.py` by using print statements. All these tests are called in the
 `main` method in the same module. Simply run the `test.py` module to view the
 results of the test functions.
 
-## Libraries used 
+## Libraries used
 Please install the following libraries in order to run the modules:
 - `Plotly`
 - `pandas`
 - `geopandas`
 - `matplotlib`
-- `seaborn` 
+- `seaborn`
 
 ## GitHub Link
 https://github.com/vianneb/cse163finalproject
--- main.py
+++ main.py
@@ -9,6 +9,10 @@ visualizations that help represent the gender wage gap.
 In order to call these methods, this file first has to
 use the geopandas and pandas libraries to load in our data.
 
+It also has the function compute_statistics, which takes our
+three data sets pertaining to gender wage gap data, and computes
+and prints summary statistics about them.
+
 Our data consists of four data sets:
 - gender-wage-gap-oecd (1).csv - a dataset from OECD that has
 columns representing the country the data is for (entity),
@@ -34,6 +38,34 @@ import pandas as pd
 import analysis
 
 
+def compute_statistics(oecd_data, glassdoor_data, management_data):
+    grouped_oecd = oecd_data.groupby('Entity')
+    print('There are', len(grouped_oecd), 'countries in the OECD data.')
+    print('The average wage gap, over all years for any country, is',
+          round(oecd_data['Gender wage gap (OECD 2017)'].mean(), 2))
+    men_glassdoor = glassdoor_data[glassdoor_data['Gender'] == 'Male']
+    women_glassdoor = glassdoor_data[glassdoor_data['Gender'] == 'Female']
+    num_men_glassdoor = len(men_glassdoor)
+    num_women_glassdoor = len(women_glassdoor)
+    print("In the Glassdoor data set, there are", num_men_glassdoor, "men",
+          "and", num_women_glassdoor, "women.")
+    print("This means there are", round(num_men_glassdoor / num_women_glassdoor, 2),
+          "times more men than women included in the dataset.")
+    men_avg_seniority = round(men_glassdoor['Seniority'].mean(), 2)
+    women_avg_seniority = round(women_glassdoor['Seniority'].mean(), 2)
+    print('For men, the average seniority was', men_avg_seniority,
+          'while for women the average seniority was', women_avg_seniority)
+    copy_management_data = management_data.copy()
+    long_column_name = '5.5.2 - Proportion of women in senior and' + \
+        ' middle management positions (%) - IC_GEN_MGTN'
+    copy_management_data = copy_management_data.rename(columns={long_column_name:
+                                              'women_in_management'})
+    print('The average proportion of women in senior and middle management',
+          'positions, for any country and across all years, is',
+          round(copy_management_data['women_in_management'].mean(), 2))
+    
+
+
 def main():
     # Load in data
     # Geo-spatial data for world countries (geopandas)
@@ -45,6 +77,9 @@ def main():
         pd.read_csv('Data/women-in-senior-and-middle-management-positions.csv')
     # Glassdoor Data on occupations and education (pandas)
     occupations_education_df = pd.read_csv('Data/glassdoor_gender_pay_gap.csv')
+    # Compute basic statistics
+    compute_statistics(country_wage_df, occupations_education_df,
+                       management_positions_df)
     # Run Analysis Functions
     analysis.countries_wage_gap_plot(country_wage_df, countries)
     analysis.occupations_wage_gap_plot(occupations_education_df)
